b262edf8583a2d6385f9972472cdef4ff98152d6
Yuto Tanaka <git@mimikun.dev>
2026-07-25 23:01:13 +0900

    Revert "chore: disable background transparency and wallpaper"
    This reverts commit ae1eeec4dc1bcd1a8f47cb3f2294d9c44ffc8325.

--- lua/config/window.lua
+++ lua/config/window.lua
@@ -1,7 +1,6 @@
 local function window(config)
   config.window_decorations = "INTEGRATED_BUTTONS|RESIZE"
-  -- NOTE: Temporarily disabled. Revert this commit to restore
-  --config.window_background_opacity = 0.93
+  config.window_background_opacity = 0.93
 end
 
 return window
